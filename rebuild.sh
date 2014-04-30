08b11238c20c6e8f57d82cf26b9d4e22408e84b3
gouf <innocent.zero@gmail.com>
2014-04-30 11:59:18 +0900

    Rename rails-app to rails-app-demo

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo docker build --no-cache --rm=true -t innocentzero/rails-app - < Dockerfile-setup
-sudo docker build --no-cache --rm=true -t innocentzero/rails-app - < Dockerfile-run
+sudo docker build --no-cache --rm=true -t innocentzero/rails-app-demo - < Dockerfile-setup
+sudo docker build --no-cache --rm=true -t innocentzero/rails-app-demo - < Dockerfile-run
--- starter.sh
+++ starter.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo docker run -d -p 3035:3000 innocentzero/rails-app
+sudo docker run -d -p 3035:3000 innocentzero/rails-app-demo
 docker ps
